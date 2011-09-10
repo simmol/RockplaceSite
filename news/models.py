3a5ab266470a3b29dd07f4c3e0f73c8fbb1a28ab
Anna Milenkova <selene@selene-notebook>
2011-09-10 18:21:12 +0300

    Minor changes (added a few spaces

--- news/models.py
+++ news/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
   
-  author = models.ForeignKey(User, related_name="article_author")
-  title = models.CharField(max_length=120)
+  author = models.ForeignKey(User, related_name = "article_author")
+  title = models.CharField(max_length = 120)
   content = models.TextField()
   published = models.BooleanField(default = False)
   pub_date = models.DateTimeField('date published')
